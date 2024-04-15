c6cb2bcbf0feff8cf1f882154246b6b9c219ac81
iCarlosAugusto <augusto.dev@proton.me>
2024-04-14 23:40:04 -0300

    refactor: :recycle: refactor modules

--- backend/src/modules/employees/employees.module.ts
+++ backend/src/modules/employees/employees.module.ts
@@ -3,10 +3,11 @@ import { EmployeesController } from './employees.controller';
 import { EmployeesService } from './employees.service';
 import { PrismaService } from 'utils/prisma.service';
 import { EmployeesRepository } from 'src/repositories/employee.repository';
+import { SquadRepository } from 'src/repositories/squad.repository';
 
 @Module({
   imports: [],
   controllers: [EmployeesController],
-  providers: [EmployeesService, EmployeesRepository, PrismaService],
+  providers: [EmployeesService, EmployeesRepository, SquadRepository, PrismaService],
 })
 export class EmployeesModule {}
--- backend/src/modules/reports/reports.module.ts
+++ backend/src/modules/reports/reports.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
+import { ReportsRepository } from 'src/repositories/reports.repository';
+import { PrismaService } from 'utils/prisma.service';
+import { EmployeesRepository } from 'src/repositories/employee.repository';
 
 @Module({
   controllers: [ReportsController],
-  providers: [ReportsService],
+  providers: [ReportsService, ReportsRepository, EmployeesRepository, PrismaService],
 })
 export class ReportsModule {}
