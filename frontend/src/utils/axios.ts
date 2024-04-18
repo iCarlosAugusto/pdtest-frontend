a77eb213976ce45dda7a761aa66d3c8bd7485995
iCarlosAugusto <augusto.dev@proton.me>
2024-04-18 02:31:56 -0300

    feat: :sparkles: added http

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosRequest = axios.create({
-  baseURL: 'ec2-3-83-103-75.compute-1.amazonaws.com:3000', // Insira a sua baseURL aqui
+  baseURL: 'http://ec2-3-83-103-75.compute-1.amazonaws.com:3000', // Insira a sua baseURL aqui
 });
 
 export default axiosRequest;
